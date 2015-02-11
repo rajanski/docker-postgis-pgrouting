8f4e3359f34564473b2d0ad4f0d6ee139dbca32f
rajanski <rajanski@users.noreply.github.com>
2015-02-11 13:13:58 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+# Add any additional setup tasks here
+
+service postgresql start 
+su postgres -c "createuser -d -s -r -l docker" 
+su postgres -c "psql postgres -c \"ALTER USER docker WITH ENCRYPTED PASSWORD 'docker'\" 
+psql postgres -c \"CREATE DATABASE docker\" && psql postgres -c \"GRANT ALL ON DATABASE docker to docker\"" 
+
+
+su postgres -c "createdb routes -U postgres -O postgres"
+su postgres -c "psql postgres -d routes -c \"create extension postgis;\"" 
+su postgres -c "psql postgres -d routes -c \"create extension pgrouting;\""
+su postgres -c "psql postgres -d routes -c \"create extension hstore;\""
+su postgres -c "psql postgres -d routes -c 'create extension \"uuid-ossp\";'"
+
+service postgresql stop

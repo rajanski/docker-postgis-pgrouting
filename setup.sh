bb503bfddd85a224acfe800e2f311ad3f53b8b3d
rajanski <rajanski@users.noreply.github.com>
2015-02-11 13:45:25 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,8 +6,11 @@
 service postgresql start 
 su postgres -c "createuser -d -s -r -l docker" 
 su postgres -c "psql postgres -c \"ALTER USER docker WITH PASSWORD 'docker'\" 
-psql postgres -c \"CREATE DATABASE docker\" && psql postgres -c \"GRANT ALL ON DATABASE docker to docker\"" 
 
+su postgres -c "createuser -d -s -r -l docker" 
+su postgres -c "psql postgres -c \"ALTER USER docker WITH ENCRYPTED PASSWORD 'docker'\"" 
+su postgres -c "psql postgres -c \"CREATE DATABASE docker\"" 
+su postgres -c "psql postgres -c \"GRANT ALL ON DATABASE docker to docker\""
 
 su postgres -c "createdb routes -U postgres -O postgres"
 su postgres -c "psql postgres -d routes -c \"create extension postgis;\"" 
